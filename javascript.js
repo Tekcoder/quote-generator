6e13f3ffc4e364a70cc9b664aebb4f1c64e2f576
Olayemi Adebayo <techwriter2022@gmail.com>
2019-11-27 12:20:33 +0100

    added the files

---
+++ javascript.js
@@ -0,0 +1,22 @@
+const newQuote = () => {
+let randomNumber = Math.floor(Math.random() * (array.length));
+document.getElementById('quoteDisplay').innerHTML = array[randomNumber];
+}
+
+let array = ['The way get started is to quit talking and begin doing. – Walt Disney',
+'The pessimist sees difficulty in every opportunity. The optimist sees opportunity in every difficulty. – Winston Churchill',
+'Don\’t let yesterday take up too much of today.” – Will Rogers',
+'You learn more from failure than from success. Don\’t let it stop you. Failure builds character.” – Unknown',
+'It\’s not whether you get knocked down, it\’s whether you get up.” – Inspirational Quote by Vince Lombardi',
+'If you are working on something that you really care about, you don\’t have to be pushed. The vision pulls you.” – Steve Jobs',
+'People who are crazy enough to think they can change the world, are the ones who do.” – Rob Siltanen',
+'Failure will never overtake me if my determination to succeed is strong enough.” – Og Mandino',
+'Entrepreneurs are great at dealing with uncertainty and also very good at minimizing risk. That’s the classic entrepreneur.” – Mohnish Pabrai',
+'We may encounter many defeats but we must not be defeated.” – Maya Angelou']
+
+
+// let array_middle = ['is to quit talking','every opportunity. The optimist sees','take up too much','from success. Don’t let it stop you.','knocked down, it’s whether','you really care about, you don’t have to be pushed.','to think they can change the world,','if my determination to succeed','uncertainty and also very good at minimizing risk.','many defeats but we','']
+
+
+
+// let array_end = ['and begin doing. – Walt Disney','opportunity in every difficulty. – Winston Churchill','of today.” – Will Rogers','Failure builds character.” – Unknown','you get up.” – Inspirational Quote by Vince Lombardi','The vision pulls you.” – Steve Jobs','are the ones who do.” – Rob Siltanen','is strong enough.” – Og Mandino','That’s the classic entrepreneur.” – Mohnish Pabrai','must not be defeated.” – Maya Angelou','','','']
